369584aaefe993d026df1b2519233932bf31efea
TJDoesCode <trevr03@gmail.com>
2017-06-25 17:15:45 -0500

    added asciitext command

---
+++ commands/fun/asciitext.js
@@ -0,0 +1,45 @@
+const commando = require('discord.js-commando');
+const oneLine = require('common-tags').oneLine;
+const ascii = require('figlet');
+
+module.exports = class AsciiTextCommand extends commando.Command {
+  constructor(client) {
+    super(client, {
+      name: 'asciitext',
+      aliases: ['ascii', 'texttoascii', 'textart', 'textasciiart'],
+      group: 'fun',
+      memberName: 'asciitext',
+      description: 'Turns text into ascii art.',
+      details: oneLine `
+        Do you like ascii art?
+        This command converts your message into ascii art.
+			`,
+      examples: ['ascii hello'],
+      args: [{
+        key: 'toAscii',
+        label: 'text',
+        prompt: 'What would you like to turn into ascii text?',
+        type: 'string',
+        infinite: false
+      }]
+    })
+  }
+
+  async run(message, args) {
+    if (args.toAscii.length > 10) return message.reply('Your message is too long!')
+    ascii(args.toAscii, {
+        horizontalLayout: 'fitted',
+        verticalLayout: 'fitted'
+      },
+      function(err, data) {
+        if (err) {
+          message.reply('Something went wrong! Contact a developer.')
+          console.error(err)
+        }
+        message.delete(1)
+        message.channel.send(data, {
+          code: 'text'
+        })
+      })
+  }
+};
