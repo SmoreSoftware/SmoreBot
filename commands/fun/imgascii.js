93570f6fb4bd0bf69b1b965c97d54bde334cf450
TJDoesCode <trevr03@gmail.com>
2017-06-25 19:21:45 -0500

    added broken imgascii command

---
+++ commands/fun/imgascii.js
@@ -0,0 +1,39 @@
+const commando = require('discord.js-commando');
+const oneLine = require('common-tags').oneLine;
+const ascii = require('image-to-ascii');
+
+module.exports = class ImgAsciiCommand extends commando.Command {
+  constructor(client) {
+    super(client, {
+      name: 'imgascii',
+      aliases: ['imgasciiart', 'imgtoascii'],
+      group: 'fun',
+      memberName: 'imgascii',
+      description: 'Turns an image into ascii art.',
+      details: oneLine `
+        Do you like ascii art? Is text not enough for you?
+        This command converts an image to ascii art.
+        Awwwww yeaah.
+			`,
+      examples: ['imgascii https://cdn.discordapp.com/attachments/324656649665118209/328609768044494849/codercat.jpg'],
+      args: [{
+        key: 'toAscii',
+        label: 'image',
+        prompt: 'What image would you like to convert to ascii art?',
+        type: 'string',
+        infinite: false
+      }]
+    })
+  }
+
+  async run(message, args) {
+    ascii(args.toAscii, (err, converted) => {
+      if (err) {
+        message.reply('Something went wrong! Contact a developer.')
+        console.error(err)
+      }
+      //message.channel.send(`${converted}`).catch(message.reply('Something went wrong! Contact a developer. \nIt\'s likely that your image was too large to send.'))
+      console.log(converted)
+    })
+  }
+};
