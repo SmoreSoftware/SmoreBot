0fcfca0761d798493205ba8d42eaed1b599d9a29
TJDoesCode <tjdoescode@gmail.com>
2017-07-10 19:44:21 -0500

    more comprehensive stats

--- commands/control/listguilds.js
+++ commands/control/listguilds.js
@@ -30,6 +30,7 @@ Name: ${guild.name}
 Owner: ${guild.owner.user.tag} (${guild.owner.id})
 Default Channel: #${guild.defaultChannel.name} (${guild.defaultChannel.id})
 Members: ${guild.members.size}
+Humans: ${guild.members.filter(u => !u.user.bot).size}
 Bots: ${guild.members.filter(u => u.user.bot).size}
 Bot percentage: ${Math.floor(guild.members.filter(u => u.user.bot).size / guild.members.size * 100)}%`)
     })
