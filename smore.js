55b4e747d7911d28f3108547d8ef9323d0ae0df6
TJ The Developer <tjdoescode@gmail.com>
2017-10-25 22:09:48 -0500

    Stupid TJ #61 (formatting)
    ESLint yelled at me and I had a spammy log so uh let's not

--- smore.js
+++ smore.js
@@ -276,12 +276,11 @@ Now on: ${client.guilds.size} servers`)
 						return
 						//eslint-disable-next-line no-else-return
 					} else {
-						console.log(row.points)
 						if (parseInt(row.points) >= 100) {
-						  let curBal = parseInt(row.balance)
-						  let newBal = curBal + 1
-						  sql.run(`UPDATE bank SET points = ${newBal} WHERE userId = ${message.author.id}`)
-						  sql.run(`UPDATE bank SET points = ${0} WHERE userId = ${message.author.id}`)
+							let curBal = parseInt(row.balance)
+							let newBal = curBal + 1
+							sql.run(`UPDATE bank SET points = ${newBal} WHERE userId = ${message.author.id}`)
+							sql.run(`UPDATE bank SET points = ${0} WHERE userId = ${message.author.id}`)
 						}
 						//eslint-disable-next-line
 						if (!cooldownUsers.includes(message.author.id)) {
