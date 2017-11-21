37259b6f3146619e60f3633ea780fcb138fae81b
TJ The Developer <tjdoescode@gmail.com>
2017-11-21 16:20:37 -0600

    fix fleave
    accomadate guild#defaultChannel deprecation

--- commands/control/fleave.js
+++ commands/control/fleave.js
@@ -3,39 +3,53 @@ const commando = require('discord.js-commando');
 const oneLine = require('common-tags').oneLine;
 
 module.exports = class FLeaveCommand extends commando.Command {
-  constructor(bot) {
-    super(bot, {
-      name: 'fleave',
-      aliases: ['forceleave', 'leaveguild', 'removeguild'],
-      group: 'control',
-      memberName: 'fleave',
-      description: 'Leaves a guild.',
-      details: oneLine `
+	constructor(bot) {
+		super(bot, {
+			name: 'fleave',
+			aliases: ['forceleave', 'leaveguild', 'removeguild'],
+			group: 'control',
+			memberName: 'fleave',
+			description: 'Leaves a guild.',
+			details: oneLine `
 				This command force leaves a guild.
         Permission locked to bot owners for security reasons.
 			`,
-      examples: ['fleave 1234567890'],
+			examples: ['fleave 1234567890'],
 
-      args: [{
-        key: 'toLeave',
-        label: 'toLeave',
-        prompt: 'What guild would you like to leave?',
-        type: 'string',
-        infinite: false
-      }],
+			args: [{
+				key: 'toLeave',
+				label: 'toLeave',
+				prompt: 'What guild would you like to leave?',
+				type: 'string',
+				infinite: false
+			}],
 
-      guarded: true
-    });
-  }
+			guarded: true
+		});
+	}
 
-  hasPermission(msg) {
-    return this.client.isOwner(msg.author);
-  }
+	hasPermission(msg) {
+		return this.client.isOwner(msg.author);
+	}
 
-  async run(message, args) {
-    let guild = this.client.guilds.get(args.toLeave)
-    guild.defaultChannel.send('**ALERT:** Your guild has been marked as an illegal guild. \nThis may be due to it being marked as a bot guild or marked as a spam guild. \nThe bot will now leave this server. \nIf you wish to speak to my developer, you may join here: https://discord.gg/6P6MNAU')
-    guild.leave()
-    message.reply('Left guild.')
-  }
+	async run(message, args) {
+		let found = 0
+		let guild = this.client.guilds.get(args.toLeave)
+		//eslint-disable-next-line array-callback-return
+		guild.channels.map((c) => {
+			if (found === 0) {
+				if (c.type === 'text') {
+					if (c.permissionsFor(this.client.user).has('VIEW_CHANNEL') === true) {
+						if (c.permissionsFor(this.client.user).has('SEND_MESSAGES') === true) {
+							c.send('**ALERT:** Your guild has been marked as an illegal guild. \nThis may be due to it being marked as a bot guild or marked as a spam guild. \nThe bot will now leave this server.')
+							c.send('If you wish to speak to my developers, you may find them here: https://discord.gg/6P6MNAU')
+							found = 1
+						}
+					}
+				}
+			}
+		})
+		guild.leave()
+		message.reply('Left guild.')
+	}
 };
