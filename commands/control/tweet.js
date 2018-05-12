9b776bf261f683b91535c7a202dc763593f5f5ee
TJ The Developer <tjdoescode@gmail.com>
2018-05-12 00:40:58 -0500

    please dear gOD WORK

--- commands/control/tweet.js
+++ commands/control/tweet.js
@@ -3,68 +3,70 @@
 const commando = require('discord.js-commando');
 const oneLine = require('common-tags').oneLine;
 const Twit = require('twit');
+const dotenv = require('dotenv');
+dotenv.load()
 const T = new Twit({
-  consumer_key: process.env.twit_conusmer_key,
-  consumer_secret: process.env.twit_consumer_secret,
-  access_token: process.env.twit_access_token,
-  access_token_secret: process.env.twit_access_token_secret
+	consumer_key: process.env.TWIT_CONSUMER_KEY,
+	consumer_secret: process.env.TWIT_CONSUMER_SECRET,
+	access_token: process.env.TWIT_ACCESS_TOKEN,
+	access_token_secret: process.env.TWIT_ACCESS_TOKEN_SECRET
 });
 /*eslint-enable camelcase*/
 
 module.exports = class TweetCommand extends commando.Command {
-  constructor(client) {
-    super(client, {
-      name: 'tweet',
-      aliases: ['sendtweet', 'twittersend'],
-      group: 'control',
-      memberName: 'tweet',
-      description: 'Sends a tweet through the official SmoreSoftware twitter.',
-      details: oneLine `
+	constructor(client) {
+		super(client, {
+			name: 'tweet',
+			aliases: ['sendtweet', 'twittersend'],
+			group: 'control',
+			memberName: 'tweet',
+			description: 'Sends a tweet through the official SmoreSoftware twitter.',
+			details: oneLine `
 				This command sends a tweet to the official SmoreSoftware twitter account.
         Permission is locked to developers.
         Duh. Did you really expect to be able to use this?
 			`,
-      examples: ['tweet hi guys it me'],
+			examples: ['tweet hi guys it me'],
 
-      args: [{
-        key: 'message',
-        label: 'message',
-        type: 'string',
-        prompt: 'What would you like to tweet? (Max 140 characters)',
-        validate: text => {
-          if (text.length < 280) return true
-          //eslint-disable-next-line newline-before-return
-          return 'Your tweet must be 280 characters or less!'
-        },
-        infinite: false
-      }],
-      guildOnly: true,
-      guarded: true
-    });
-  }
+			args: [{
+				key: 'message',
+				label: 'message',
+				type: 'string',
+				prompt: 'What would you like to tweet? (Max 140 characters)',
+				validate: text => {
+					if (text.length < 280) return true
+					//eslint-disable-next-line newline-before-return
+					return 'Your tweet must be 280 characters or less!'
+				},
+				infinite: false
+			}],
+			guildOnly: true,
+			guarded: true
+		});
+	}
 
-  hasPermission(msg) {
-    return this.client.isOwner(msg.author);
-  }
+	hasPermission(msg) {
+		return this.client.isOwner(msg.author);
+	}
 
-  //eslint-disable-next-line class-methods-use-this
-  async run(message, args) {
-    let tweet = {
-      status: `${args.message}
+	//eslint-disable-next-line class-methods-use-this
+	async run(message, args) {
+		let tweet = {
+			status: `${args.message}
 -${message.author.username}`
-    }
+		}
 
-    //eslint-disable-next-line no-use-before-define
-    T.post('statuses/update', tweet, tweeted)
+		//eslint-disable-next-line no-use-before-define
+		T.post('statuses/update', tweet, tweeted)
 
-    function tweeted(err) {
-      if (err) {
-        console.error(err)
-        message.reply('There was an error! Contact a JS dev. Was your tweet too long?')
+		function tweeted(err) {
+			if (err) {
+				console.error(err)
+				message.reply('There was an error! Contact a JS dev. Was your tweet too long?')
 
-        return
-      }
-      message.reply('Tweet sent successfully.')
-    }
-  }
+				return
+			}
+			message.reply('Tweet sent successfully.')
+		}
+	}
 };
