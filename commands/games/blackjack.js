0482252cfd700b926808dafc8fffbb92df398448
TJ The Developer <tjdoescode@gmail.com>
2018-06-11 20:10:28 -0500

    fix blackjack?

--- commands/games/blackjack.js
+++ commands/games/blackjack.js
@@ -1,6 +1,9 @@
-/*eslint-disable*/
-const { Command } = require('discord.js-commando');
-const { stripIndents } = require('common-tags');
+const {
+	Command
+} = require('discord.js-commando');
+const {
+	stripIndents
+} = require('common-tags');
 
 const Blackjack = require('../../structures/games/Blackjack');
 const Currency = require('../../structures/currency/Currency');
@@ -19,36 +22,36 @@ module.exports = class BlackjackCommand extends Command {
 				duration: 30
 			},
 
-			args: [
-				{
-					key: 'bet',
-					prompt: `How many ${Currency.textPlural} do you want to bet?\n`,
-					type: 'integer',
-					validate: async (bet, msg) => {
-						bet = parseInt(bet);
-						const balance = await Currency.getBalance(msg.author.id);
-						if (balance < bet) {
-							return `
+			args: [{
+				key: 'bet',
+				prompt: `How many ${Currency.textPlural} do you want to bet?\n`,
+				type: 'integer',
+				validate: async (bet, msg) => {
+					bet = parseInt(bet);
+					const balance = await Currency.getBalance(msg.author.id);
+					if (balance < bet) {
+						return `
 								You don't have enough ${Currency.textPlural}!
 								Your current account balance is ${Currency.convert(balance)}.
 								Please specify a valid amount of ${Currency.textPlural}.
 							`;
-						}
-						if (![100, 200, 300, 400, 500, 1000].includes(bet)) {
-							return `
+					}
+					if (![100, 200, 300, 400, 500, 1000].includes(bet)) {
+						return `
 								please choose \`100, 200, 300, 400, 500, 1000\` for your bet.
 							`;
-						}
-						return true;
 					}
+					return true;
 				}
-			]
+			}]
 		});
 	}
 
-	async run(msg, { bet }) {
+	async run(msg, {
+		bet
+	}) {
 		if (Blackjack.gameExists(msg.author.id)) {
-			return msg.reply(`You can't start 2 games of blackjack at the same time.`);
+			return msg.reply('You can\'t start 2 games of blackjack at the same time.');
 		}
 
 		const blackjack = new Blackjack(msg);
@@ -62,12 +65,12 @@ module.exports = class BlackjackCommand extends Command {
 
 			if (Blackjack.handValue(playerHand) !== 'Blackjack') {
 				playerHands = await this.getFinalHand(msg, playerHand, dealerHand, balance, bet, blackjack);
-				const result = this.gameResult(Blackjack.handValue(playerHands[0]), 0);
+				const result = this.gameResult(Blackjack.handValue(playerHands[0]), 0, msg);
 				const noHit = playerHands.length === 1 && result === 'bust';
 
-				while ((Blackjack.isSoft(dealerHand)
-					|| Blackjack.handValue(dealerHand) < 17)
-					&& !noHit) { // eslint-disable-line no-unmodified-loop-condition
+				while ((Blackjack.isSoft(dealerHand) ||
+						Blackjack.handValue(dealerHand) < 17) &&
+					!noHit) { // eslint-disable-line no-unmodified-loop-condition
 					blackjack.hit(dealerHand);
 				}
 			} else {
@@ -82,27 +85,31 @@ module.exports = class BlackjackCommand extends Command {
 			const embed = {
 				title: `Blackjack | ${msg.member.displayName}`,
 				fields: [],
-				footer: { text: blackjack.cardsRemaining() ? `Cards remaining: ${blackjack.cardsRemaining()}` : `Shuffling` }
+				footer: {
+					text: blackjack.cardsRemaining() ? `Cards remaining: ${blackjack.cardsRemaining()}` : 'Shuffling'
+				}
 			};
 
 			playerHands.forEach((hand, i) => {
 				const playerValue = Blackjack.handValue(hand);
-				const result = this.gameResult(playerValue, dealerValue);
+				const result = this.gameResult(playerValue, dealerValue, msg);
 
 				if (result !== 'bust') hideHoleCard = false;
 
-				const lossOrGain = Math.floor((['loss', 'bust'].includes(result)
-					? -1 : result === 'push'
-						? 0 : 1) * (hand.doubled
-						? 2 : 1) * (playerValue === 'Blackjack'
-						? 1.5 : 1) * bet);
+				/*eslint-disable*/
+				const lossOrGain = Math.floor((['loss', 'bust'].includes(result) ?
+					-1 : result === 'push' ?
+					0 : 1) * (hand.doubled ?
+					2 : 1) * (playerValue === 'Blackjack' ?
+					1.5 : 1) * bet);
+				/*eslint-enable*/
 
 				winnings += lossOrGain;
 				const soft = Blackjack.isSoft(hand);
 				/* eslint-disable max-len */
 				embed.fields.push({
 					name: playerHands.length === 1 ? '**Your hand**' : `**Hand ${i + 1}**`,
-					value: stripIndents`
+					value: stripIndents `
 						${hand.join(' - ')}
 						Value: ${soft ? 'Soft ' : ''}${playerValue}
 						Result: ${result.replace(/(^\w|\s\w)/g, ma => ma.toUpperCase())}${result !== 'push' ? `, ${lossOrGain}` : `, ${Currency.plural} back`}
@@ -119,31 +126,42 @@ module.exports = class BlackjackCommand extends Command {
 
 			embed.fields.push({
 				name: '**Dealer hand**',
-				value: stripIndents`
+				value: stripIndents `
 					${hideHoleCard ? `${dealerHand[0]} - XX` : dealerHand.join(' - ')}
 					Value: ${hideHoleCard ? Blackjack.handValue([dealerHand[0]]) : dealerValue}
 				`
 			});
 
+			/*eslint-disable*/
 			embed.color = winnings > 0 ? 0x009900 : winnings < 0 ? 0x990000 : undefined;
 			embed.description = `You ${winnings === 0
 				? 'broke even' : `${winnings > 0
 					? 'won' : 'lost'} ${Math.abs(winnings)}`}`;
-
-			if (result !== 'bust') Currency.addBalance(msg.author.id, winnings);
-			else Currency.removeBalance(msg.author.id, winnings);
+			/*eslint-enable*/
 
 			return msg.embed(embed);
 		});
 	}
 
-	gameResult(playerValue, dealerValue) {
-		if (playerValue > 21) return 'bust';
-		if (dealerValue > 21) return 'dealer bust';
-		if (playerValue === dealerValue) return 'push';
-		if (playerValue === 'Blackjack' || playerValue > dealerValue) return 'win';
+	//eslint-disable-next-line class-methods-use-this
+	awardMoney(result, msg) {
+		if (result !== 'bust') Currency.addBalance(msg.author.id, result);
+		else Currency.removeBalance(msg.author.id, result);
+	}
 
-		return 'loss';
+	//eslint-disable-next-line class-methods-use-this
+	gameResult(playerValue, dealerValue, msg) {
+		let result;
+		if (playerValue > 21) result = 'bust';
+		if (dealerValue > 21) result = 'dealer bust';
+		if (playerValue === dealerValue) result = 'push';
+		if (playerValue === 'Blackjack' || playerValue > dealerValue) result = 'win';
+
+		if (!result) result = 'loss'
+
+		this.awardMoney(result, msg);
+
+		return result;
 	}
 
 	//eslint-disable-next-line class-methods-use-this
@@ -158,37 +176,38 @@ module.exports = class BlackjackCommand extends Command {
 				if (currentHand.length === 1) blackjack.hit(currentHand);
 				if (Blackjack.handValue(currentHand) === 'Blackjack') {
 					nextHand();
+					//eslint-disable-next-line no-continue
 					continue;
 				}
 				if (Blackjack.handValue(currentHand) >= 21) {
 					nextHand();
+					//eslint-disable-next-line no-continue
 					continue;
 				}
 				if (currentHand.doubled) {
 					blackjack.hit(currentHand);
 					nextHand();
+					//eslint-disable-next-line no-continue
 					continue;
 				}
 
 				const canDoubleDown = balance >= totalBet + bet && currentHand.length === 2;
-				const canSplit = balance >= totalBet + bet
-					&& Blackjack.handValue([currentHand[0]]) === Blackjack.handValue([currentHand[1]])
-					&& currentHand.length === 2;
+				const canSplit = balance >= totalBet + bet &&
+					Blackjack.handValue([currentHand[0]]) === Blackjack.handValue([currentHand[1]]) &&
+					currentHand.length === 2;
 
+				/*eslint-disable*/
 				await msg.embed({ // eslint-disable-line no-await-in-loop
 					title: `Blackjack | ${msg.member.displayName}`,
-					description: !canDoubleDown && !canSplit
-						? 'Type `hit` to draw another card or `stand` to pass.'
-						: `Type \`hit\` to draw another card, ${canDoubleDown
+					description: !canDoubleDown && !canSplit ?
+						'Type `hit` to draw another card or `stand` to pass.' : `Type \`hit\` to draw another card, ${canDoubleDown
 							? '`double down` to double down, '
 							: ''}${canSplit
 							? '`split` to split, ' : ''}or \`stand\` to pass.`,
-					fields: [
-						{
-							name: hands.length === 1
-								? '**Your hand**'
-								: `**Hand ${hands.indexOf(currentHand) + 1}**`,
-							value: stripIndents`
+					fields: [{
+							name: hands.length === 1 ?
+								'**Your hand**' : `**Hand ${hands.indexOf(currentHand) + 1}**`,
+							value: stripIndents `
 								${currentHand.join(' - ')}
 								Value: ${Blackjack.isSoft(currentHand) ? 'Soft ' : ''}${Blackjack.handValue(currentHand)}
 							`,
@@ -196,26 +215,31 @@ module.exports = class BlackjackCommand extends Command {
 						},
 						{
 							name: '**Dealer hand**',
-							value: stripIndents`
+							value: stripIndents `
 								${dealerHand[0]} - XX
 						 		Value: ${Blackjack.isSoft([dealerHand[0]]) ? 'Soft ' : ''}${Blackjack.handValue([dealerHand[0]])}
 							`,
 							inline: true
 						}
 					],
-					footer: { text: blackjack.cardsRemaining() ? `Cards remaining: ${blackjack.cardsRemaining()}` : `Shuffling` }
+					footer: {
+						text: blackjack.cardsRemaining() ? `Cards remaining: ${blackjack.cardsRemaining()}` : `Shuffling`
+					}
 				});
+				/*eslint-enable*/
 
-				const responses = await msg.channel.awaitMessages(msg2 => // eslint-disable-line no-await-in-loop
+				/*eslint-disable*/
+				const responses = await msg.channel.awaitMessages(msg2 =>
 					msg2.author.id === msg.author.id && (
-						msg2.content === 'hit'
-						|| msg2.content === 'stand'
-						|| (msg2.content === 'split' && canSplit)
-						|| (msg2.content === 'double down' && canDoubleDown)
+						msg2.content === 'hit' ||
+						msg2.content === 'stand' ||
+						(msg2.content === 'split' && canSplit) ||
+						(msg2.content === 'double down' && canDoubleDown)
 					), {
-					maxMatches: 1,
-					time: 20e3
-				});
+						maxMatches: 1,
+						time: 20e3
+					});
+				/*eslint-enable*/
 
 				if (responses.size === 0) break;
 				const action = responses.first().content.toLowerCase();
