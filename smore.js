5bc90cc0395463c6b607d94e8f3ea65c9de53e2b
TJ The Developer <tjdoescode@gmail.com>
2018-07-04 20:38:17 -0500

    Stupid TJ #67 (on guildDelete chan id)

--- smore.js
+++ smore.js
@@ -266,7 +266,7 @@ Members: ${guild.members.size}
 Bots: ${guild.members.filter(u => u.user.bot).size} (${Math.floor(guild.members.filter(u => u.user.bot).size / guild.members.size * 100)}%)
 Humans: ${guild.members.filter(u => !u.user.bot).size} (${Math.floor(guild.members.filter(u => !u.user.bot).size / guild.members.size * 100)}%)
 Now on: ${client.guilds.size} servers`)
-    client.channels.get('330701184698679307').send(`Existing guild left:
+    client.channels.get('402318969379225618').send(`Existing guild left:
 Guild: ${guild.id}
 Name: ${guild.name}
 Owner: ${guild.owner.user.tag} (${guild.owner.id})
