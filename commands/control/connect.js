8666f175849ba9c0f2018c7c2b4a6d55f2eb622e
TJ The Developer <tjdoescode@gmail.com>
2018-07-09 19:54:13 -0500

    git sucks

--- commands/control/connect.js
+++ commands/control/connect.js
@@ -10,8 +10,8 @@ module.exports = class ConnectCommand extends commando.Command {
       group: 'support',
       memberName: 'connect',
       description: 'Opens a support connection to a channel.',
-      details: oneLine`
-				Did you miss a support call?
+      details: oneLine `
+				Did you miss a supprt call?
 				Use this command to open a support connection to any channel and assist them.
 			`,
       examples: ['connect 322450311597916172'],
@@ -23,45 +23,46 @@ module.exports = class ConnectCommand extends commando.Command {
         type: 'string',
         infinite: false
       }],
+
       guildOnly: true,
       guarded: true
-    });
+    })
   }
 
   hasPermission(msg) {
-    return this.client.isOwner(msg.author) || allowed.general.includes(msg.author.id);
+    return this.client.isOwner(msg.author) || allowed.support.includes(msg.author.id);
   }
 
-  run(message, args) {
-    const chan = this.client.channels.get(args.chan);
-    let supportChan = '322450311597916172';
-    supportChan = this.client.channels.get(supportChan);
-    let isEnabled = true;
+  async run(message, args) {
+    const chan = this.client.channels.get(args.chan)
+    let supportChan = '322450311597916172'
+    supportChan = this.client.channels.get(supportChan)
+    let isEnabled = true
 
-    chan.send('**ALERT:** Incoming call from SmoreSoftware Support!');
-    supportChan.send(`Connecting to "#${chan.name}" {${chan.id}) on the guild "${chan.guild.name}" (${chan.guild.id})`);
-    let sent = 0;
-    // ! Remove this listener, it's leaking memory
+    chan.send('**ALERT:** Incoming call from SmoreSoftware Support!')
+    supportChan.send(`Connecting to "#${chan.name}" {${chan.id}) on the guild "${chan.guild.name}" (${chan.guild.id})`)
+    let sent = 0
     this.client.on('message', message => {
       if (sent === 0) {
-        supportChan.send('Connected. Do `call end` at any time to end the call.');
-        sent = 1;
+        //eslint-disable-next-line no-useless-escape
+        supportChan.send('Connected. Do \`call end\` at any time to end the call.')
+        sent = 1
       }
 
-      const contact = () => {
-        if (isEnabled === false) return;
-        if (message.author.id === '290228059599142913') return;
+      function contact() {
+        if (isEnabled === false) return
+        if (message.author.id === '290228059599142913') return
         if (message.content.startsWith('call end')) {
-          message.channel.send(':x: Call has been hung up.');
-          if (message.channel.id === chan.id) supportChan.send(':x: The call was ended from the other side.');
-          if (message.channel.id === supportChan) chan.send(':x: The call was ended from the other side.');
+          message.channel.send(':x: Call has been hung up.')
+          if (message.channel.id === chan.id) supportChan.send(':x: The call was ended from the other side.')
+          if (message.channel.id === supportChan) chan.send(':x: The call was ended from the other side.')
 
-          return isEnabled = false;
+          return isEnabled = false
         }
-        if (message.channel.id === chan.id) supportChan.send(`:telephone_receiver: **${message.author.tag}**: ${message.content}`);
-        if (message.channel.id === supportChan.id) chan.send(`:star: ${message.content}`);
-      };
-      contact();
-    });
+        if (message.channel.id === chan.id) supportChan.send(`:telephone_receiver: **${message.author.tag}**: ${message.content}`)
+        if (message.channel.id === supportChan.id) chan.send(`:star: ${message.content}`)
+      }
+      contact()
+    })
   }
 };
